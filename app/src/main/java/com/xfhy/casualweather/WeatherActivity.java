69ece0fefaa1de1fb5652d9ed3e463af3e80d9c3
xfhy <1319504383@qq.com>
2017-03-12 10:25:56 +0800

    加入天气背景每日换图,沉浸式状态栏

--- app/src/main/java/com/xfhy/casualweather/WeatherActivity.java
+++ app/src/main/java/com/xfhy/casualweather/WeatherActivity.java
@@ -2,6 +2,8 @@ package com.xfhy.casualweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
@@ -19,6 +21,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.xfhy.casualweather.bean.json.Forecast;
 import com.xfhy.casualweather.bean.json.Weather;
 import com.xfhy.casualweather.util.HttpUtil;
@@ -44,6 +47,10 @@ public class WeatherActivity extends AppCompatActivity {
      * 获取天气信息成功
      */
     private static final int REQUEST_WEATHER_SUCCESS = 2;
+    /**
+     * 加载图片
+     */
+    private static final int LOAD_BING_PIC = 3;
 
     public DrawerLayout drawerLayout;
 
@@ -75,26 +82,30 @@ public class WeatherActivity extends AppCompatActivity {
 
     private ImageView bingPicImg;
 
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case REQUEST_WEATHER_FAILD:
                     Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                     break;
                 case REQUEST_WEATHER_SUCCESS:
-                    Weather weather = (Weather) msg.obj;
-                    if(weather != null  && weather.status.equals("ok")){
+                    Weather weather = (Weather) msg.obj;   //将天气信息保存到SharedPreferences中
+                    if (weather != null && weather.status.equals("ok")) {
                         SharedPreferences.Editor editor = PreferenceManager.
                                 getDefaultSharedPreferences(WeatherActivity.this).edit();
                         Bundle bundle = msg.getData();
                         String responseText = bundle.getString("response_text");
-                        editor.putString("weather",responseText);
+                        editor.putString("weather", responseText);
                         editor.apply();
-                        showWeatherInfo(weather);
+                        showWeatherInfo(weather);  //显示天气信息
                     } else {
                         Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                     }
+                    break;
+                case LOAD_BING_PIC:  //加载图片
+                    String bingPic = (String) msg.obj;
+                    Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
                     break;
                 default:
                     break;
@@ -102,11 +113,21 @@ public class WeatherActivity extends AppCompatActivity {
         }
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        //状态栏沉浸式效果    下面的方法只适合Android 5.0 以上系统
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();  //改变系统UI的显示
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE); //表示活动的布局会显示在状态栏上
+            getWindow().setStatusBarColor(Color.TRANSPARENT);    //将状态栏设置成透明
+        }
+
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
         titleCity = (TextView) findViewById(R.id.title_city);
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
@@ -119,9 +140,10 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
 
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String weatherString = prefs.getString("weather",null);
-        if(weatherString != null){
+        String weatherString = prefs.getString("weather", null);
+        if (weatherString != null) {
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
             showWeatherInfo(weather);
@@ -132,10 +154,46 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
 
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        } else {
+            loadBingPic();
+        }
+
+    }
+
+    /**
+     * 加载必应每日一图
+     */
+    private void loadBingPic() {
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.
+                        getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+
+                //异步,加载图片
+                Message msg = Message.obtain();
+                msg.what = LOAD_BING_PIC;
+                msg.obj = bingPic;
+                handler.sendMessage(msg);
+            }
+        });
     }
 
     /**
      * 根据天气id请求城市天气信息
+     *
      * @param weatherId
      */
     private void requestWeather(String weatherId) {
@@ -154,17 +212,18 @@ public class WeatherActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String responseText = response.body().string();
-                LogUtil.i(TAG,responseText);
+                LogUtil.i(TAG, responseText);
                 Weather weather = Utility.handleWeatherResponse(responseText);
                 Message msg = Message.obtain();
                 msg.what = REQUEST_WEATHER_SUCCESS;
                 msg.obj = weather;
                 Bundle bundle = new Bundle();
-                bundle.putString("response_text",responseText);
+                bundle.putString("response_text", responseText);
                 msg.setData(bundle);
                 handler.sendMessage(msg);
             }
         });
+        loadBingPic();
     }
 
     /**
